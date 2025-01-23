3639df17969858596e5812f8ee02910e084cf071
Thatcher <thatcherwer120@gmail.com>
2025-01-22 20:53:31 -0600

    Updated TestRectangleSquareCircle
    Moved the tests area and permiter loops for squares, circles, and rectangles to TestRectangleSquareCircle
    
    Also renamed TestRectangleSquare to TestRectangleSquareCircle and stated that answers are in LabTask2.txt

--- src/TestRectangleSquareCircle.java
+++ src/TestRectangleSquareCircle.java
@@ -1,4 +1,6 @@
-public class TestRectangleSquare {
+import java.util.ArrayList;
+
+public class TestRectangleSquareCircle {
 
 	public static void main(String[] args) {
 		// Liskov substitution principle
@@ -49,7 +51,34 @@ public class TestRectangleSquare {
 		
 		// Which of the classes should be a subtype of the other one? 
 		// Use the Liskov substitution principle to answer this question
+		// Answered in LabTask2.txt
 		
+
+		//Lab task 2, Question2C
+		
+		ArrayList<Shape> shapes = new ArrayList<>();
+		//Squares
+		for(int i = 0; i < 5; i++)
+		{
+			int x = (int) (Math.random() * 10) + 1;
+			shapes.add(new Square(x));
+		}
+		for(int i = 0; i < 5; i++)
+		{
+			int x = (int) (Math.random() * 10) + 1;
+			int y = (int) (Math.random() * 10) + 1;
+			shapes.add(new Rectangle(x, y));
+		}
+		for(int i = 0; i < 5; i++)
+		{
+			int x = (int) (Math.random() * 5) + 1;
+			shapes.add(new Circle(x));
+		}
+		for(Shape shape : shapes)
+		{
+			System.out.println("Shape's area: " + shape.area() + "\nShape's perimeter: " + shape.perimeter());
+		}
+
 	}
 
 }
